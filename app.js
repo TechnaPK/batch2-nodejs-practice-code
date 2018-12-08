1052c4016247aa2f5c2238f302bc6ca07e6bedf4
Umar Ahmad <s.umarahmad@gmail.com>
2018-12-08 12:42:19 +0500

    Express Static Server, Accessing HTML Form Data and URL Params

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
-const express = require('express');
-
+var express = require('express');
+var bodyParser = require('body-parser')
 var server = express()
 
+server.use( bodyParser.urlencoded() )
+
 server.use((req, res, next)=>{
-    console.log(new Date())
+    // console.log(new Date())
     next()
 })
 
-server.get('/user', (req, res)=>{
-    res.send('user page')
+server.use( express.static('./frontend') )
+
+server.get('/user/:name/:lname', (req, res)=>{
+    res.send('My name is ' + req.params.name + " " + req.params.lname)
 })
 
 server.get('/hello', (req, res)=>{
     res.send('Hello World')
 })
 
-server.listen(8000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var http = require('http');
-
-// http.createServer(function (req, res) {
-
-//     res.write("Hello World!")
-//     res.end();
+server.get('/search', (req, res)=>{
     
-// }).listen(8000);
+    res.send( req.query )
+
+})
+
+server.post('/login', (req, res)=>{
+    
+    res.send( req.body.username + ' ' + req.body.password  )
+    res.send( req.body.password )
+
+})
+
+server.listen(8000, () => { console.log("Server successfully started.") } )
+
+
+
+
+
+
+// server.get('/search', (req, res) => {
+//     console.log( req.query )
+//     res.end("Search Form request recieved.")
+// })
+
+// server.post('/login', (req, res)=>{
+//     console.log( req.body )
+//     res.end("Login Form request recieved.")
+// })
