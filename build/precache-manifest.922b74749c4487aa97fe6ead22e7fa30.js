16924dc9ad28c019a4975978298b68f6e70f9bc2
Umar Ahmad <s.umarahmad@gmail.com>
2019-01-26 17:08:28 +0500

    Removed build folder from gitignore

---
+++ build/precache-manifest.922b74749c4487aa97fe6ead22e7fa30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aaafdfdd3e088e7da115",
+    "url": "/static/js/main.aaafdfdd.chunk.js"
+  },
+  {
+    "revision": "de80605f5e2b8431d6e4",
+    "url": "/static/js/1.de80605f.chunk.js"
+  },
+  {
+    "revision": "aaafdfdd3e088e7da115",
+    "url": "/static/css/main.dc9494df.chunk.css"
+  },
+  {
+    "revision": "e4a12fc9f103866425f39a3fd7a28350",
+    "url": "/index.html"
+  }
+];
